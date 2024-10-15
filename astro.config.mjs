b930db9fd2497a3e6c9d7176ffeb59e00523da4a
Zach <101913796+YTZacharyPlayz@users.noreply.github.com>
2024-10-15 19:05:35 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/YTZacharyPlayz/NHA-Autopilot',
 			},
 			sidebar: [
 				{
