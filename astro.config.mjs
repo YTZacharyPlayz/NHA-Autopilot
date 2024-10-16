6e581a322e4d117d6cb88f1c3e7e5abef293dcab
Zach <101913796+YTZacharyPlayz@users.noreply.github.com>
2024-10-16 16:45:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ytzacharyplayz.github.io',
+	site: '<https://ytzacharyplayz.github.io',>
 	base: '/NHA-Autopilot',
 	integrations: [
 		starlight({
