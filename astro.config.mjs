a9f0f8850a9e58b608a4bb3d982efedbc62f61ed
Zach <101913796+YTZacharyPlayz@users.noreply.github.com>
2024-10-16 16:56:28 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
+// <https://astro.build/config>
 export default defineConfig({
-	site: '<https://ytzacharyplayz.github.io',>
-	base: '/NHA-Autopilot',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/YTZacharyPlayz/NHA-Autopilot',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: '<https://ytzacharyplayz.github.io',>
+    base: '/NHA-Autopilot',
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: {
+                github: '<https://github.com/YTZacharyPlayz/NHA-Autopilot',>
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
 });
