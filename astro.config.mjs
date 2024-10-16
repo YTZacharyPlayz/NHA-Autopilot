bbcfe80e942ae2c47ea4d1871a1691c5025cb125
Zach <101913796+YTZacharyPlayz@users.noreply.github.com>
2024-10-16 20:29:13 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // <https://astro.build/config>
 export default defineConfig({
+<<<<<<< HEAD
     site: 'https://ytzacharyplayz.github.io',
     base: '/NHA-Autopilot',
     integrations: [
@@ -27,4 +28,29 @@ export default defineConfig({
             ],
         }),
     ],
+=======
+	site: 'https://ytzacharyplayz.github.io/NHA-Autopilot',
+	base: 'NHA-Autopilot',
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/YTZacharyPlayz/NHA-Autopilot',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+>>>>>>> parent of 0a85a17 (Update astro.config.mjs)
 });
